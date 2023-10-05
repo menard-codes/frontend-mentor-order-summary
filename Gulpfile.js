1bbeac6d5b42d30b7a5c865db85d3d8b17acec62
Menard Maranan <menardmaranan.codes@gmail.com>
2023-10-05 23:06:50 +0800

    styled the site

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,24 +1,26 @@
-const { src, dest, series, watch } = require("gulp");
-const scss = require("gulp-sass")(require("sass"));
-const purgecss = require("gulp-purgecss");
-const autoprefixer = require("gulp-autoprefixer");
-const cssMinify = require("gulp-clean-css");
+const { src, dest, series, watch } = require('gulp');
+const scss = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
+const autoprefixer = require('gulp-autoprefixer');
+const cssMinify = require('gulp-clean-css');
 
 function cssTasks() {
-  return src("./scss/*.scss")
-    .pipe(scss())
-    .pipe(
-      purgecss({
-        content: ["./index.html"],
-      })
-    )
-    .pipe(autoprefixer("last 2 versions"))
-    .pipe(cssMinify())
-    .pipe(dest("./dist/css"));
+  return (
+    src('./scss/*.scss')
+      .pipe(scss())
+      // .pipe(
+      //   purgecss({
+      //     content: ['./index.html'],
+      //   })
+      // )
+      .pipe(autoprefixer('last 2 versions'))
+      .pipe(cssMinify())
+      .pipe(dest('./dist/css'))
+  );
 }
 
 function watchTask() {
-  watch(["./scss/**/*.scss", "./index.html"], series(cssTasks));
+  watch(['./scss/**/*.scss', './index.html'], series(cssTasks));
 }
 
 exports.default = series(cssTasks, watchTask);
